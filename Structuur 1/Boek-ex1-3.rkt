b682dd4648dbe582c13ea829d776f034f987504f
rroemers <rob@roemers.be>
2014-10-03 16:53:25 +0200

    fixed solution

--- Structuur 1/Boek-ex1-3.rkt
+++ Structuur 1/Boek-ex1-3.rkt
@@ -1,11 +1,16 @@
 ;ex 1.3: Define a procedure that takes three numbers as arguments and returns the sum of the squares
 ;of the two larger numbers.
 
-;define procedure
-(define (sum-of-sq a b c)
-;sort for largest/smallest nr
-  ((define high a) (define low b)
-                   (cond (and? (> high low) (> c low) (display a))
-                         (else (display b)))))
-;define square procedure
-;sum squares of results
+;define lower procedures
+;Square
+(define (sq s) (* s s))
+;Sum of Squares
+(define (sum-sq a b) (+ (sq a) (sq b)))
+;sort for 2 large out of 3 and apply sum of squares
+(define (sort x y z)
+  (cond ((and (<= x y) (<= x z) (sum-sq y z)))
+        ((and (<= y x) (<= y z) (sum-sq x z)))
+        ((and (<= z y) (<= z x) (sum-sq y x)))))
+
+(define (sum-of-squares a b c) (sort a b c))
+
---
+++ Structuur 1/Boek-ex1-3.rkt~
@@ -0,0 +1,11 @@
+;ex 1.3: Define a procedure that takes three numbers as arguments and returns the sum of the squares
+;of the two larger numbers.
+
+;define procedure
+(define (sum-of-sq a b c)
+;sort for largest/smallest nr
+  ((define high a) (define low b)
+                   (cond (and? (> high low) (> c low) (display a))
+                         (else (display b)))))
+;define square procedure
+;sum squares of results

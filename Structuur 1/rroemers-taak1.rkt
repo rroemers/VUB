28092320df21ba1d5ac2fe90438f60a4010e17ff
rroemers <rob@roemers.be>
2014-10-04 17:58:15 +0200

    work

---
+++ Structuur 1/rroemers-taak1.rkt
@@ -0,0 +1,20 @@
+#lang racket
+(#%require racket/trace)
+
+;Taak 1: Structuur en interpretatie van computerprogrammas
+;Rob Roemers - Rolnummer: 0517555
+;Oktober 2014
+
+;1) Sinus
+;A) Uitwerking
+
+
+
+
+;B) Opgaves:
+;(calc-sin 0 10)
+;0
+;(calc-sin (/ 3.1415 4) 10)
+;0.7070904020014415
+;(calc-sin (/ 3.1415 2) 10)
+;0.9999999989269138
